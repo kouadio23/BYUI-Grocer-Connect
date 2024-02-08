66b6a2eab76e9e7e652d77ac841f5b10c96912ae
Kouadio Kouao <kouaokouadio@gmail.com>
2024-02-07 17:13:00 -0700

    dataModelSetting

--- byuiGrocerConnect/models.py
+++ byuiGrocerConnect/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
