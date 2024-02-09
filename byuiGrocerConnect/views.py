a504cb1688a9069b68b0ba9c2242ff133afc81d9
Kouadio Kouao <kouaokouadio@gmail.com>
2024-02-08 21:02:10 -0700

    Statarted

--- byuiGrocerConnect/views.py
+++ byuiGrocerConnect/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from byuiGrocerConnect.models import Product
+
 # Create your views here.
 def index(response):
     return render(response, "byuiGrocerConnect/base.html", {})
 
 def home(response):
-    return render(response, "byuiGrocerConnect/home.html", {})
+    products = Product.objects.filter(is_featured=True)  # or however you determine featured products
+    return render(response, "byuiGrocerConnect/home.html", {'products': products})
+
+def features(response):
+    products = Product.objects.filter(is_featured=True)  # or however you determine featured products
+    return render(response, "byuiGrocerConnect/features.html", {'products': products})
