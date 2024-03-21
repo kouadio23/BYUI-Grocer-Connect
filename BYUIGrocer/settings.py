f634287aeb38352183f10e78304df75dde1ae4f4
Kouadio Kouao <kouaokouadio@gmail.com>
2024-03-20 20:30:27 -0600

    vhmfg

--- BYUIGrocer/settings.py
+++ BYUIGrocer/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'byuiGrocerConnect.apps.ByuigrocerconnectConfig',
-    'django_extensions',
+    # 'django_extensions',
 ]
 
 MIDDLEWARE = [
