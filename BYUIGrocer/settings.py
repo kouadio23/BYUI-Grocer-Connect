97a58e558dbd2e12d7cc28d6f600013e74b08a59
Kouadio Kouao <kouaokouadio@gmail.com>
2024-03-20 20:02:40 -0600

    hjkuy

--- BYUIGrocer/settings.py
+++ BYUIGrocer/settings.py
@@ -50,7 +50,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Application definition
+# Application definition.
 
 INSTALLED_APPS = [
     'django.contrib.admin',
